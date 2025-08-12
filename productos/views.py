6f5176a929fee5e33be4036b5e73631332480a4d
Jose Angel Victorio Escobar <123581444+JoseAV3567@users.noreply.github.com>
2025-08-11 22:25:59 -0600

    Update views.py

--- productos/views.py
+++ productos/views.py
@@ -11,31 +11,29 @@ from .models import Productos
 from .forms import ProductoForm
 
 
-# Vista para LEER (Listar todos los productos)
+
 class ProductoListView(ListView):
     model = Productos
-    template_name = "productos/templates/lista_productos.html"  # Plantilla a usar
-    context_object_name = "productos"  # Nombre para la lista en la plantilla
+    template_name = "productos/templates/lista_productos.html" 
+    context_object_name = "productos" 
 
 
-# Vista para LEER (Ver el detalle de un solo producto)
+
 class ProductoDetailView(DetailView):
     model = Productos
     template_name = "productos/templates/detalle_producto.html"
     context_object_name = "producto"
 
 
-# Vista para CREAR un nuevo producto
+
 class ProductoCreateView(CreateView):
     model = Productos
-    form_class = ProductoForm  # Usa nuestro formulario personalizado
+    form_class = ProductoForm 
     template_name = "productos/templates/formulario_producto.html"
-    # URL a la que se redirige si la creación es exitosa
-    # Usamos reverse_lazy para que la URL se evalúe cuando sea necesario
     success_url = reverse_lazy("lista-productos")
 
 
-# Vista para ACTUALIZAR un producto existente
+
 class ProductoUpdateView(UpdateView):
     model = Productos
     form_class = ProductoForm
@@ -43,7 +41,6 @@ class ProductoUpdateView(UpdateView):
     success_url = reverse_lazy("lista-productos")
 
 
-# Vista para BORRAR un producto
 class ProductoDeleteView(DeleteView):
     model = Productos
     template_name = "productos/templates/confirmar_borrado.html"
